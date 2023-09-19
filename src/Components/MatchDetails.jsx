ffdb7d2bb0b4c7ac88ae33ca5c67883c3ec87548
Lucas Guidi <lucasrguidi@gmail.com>
2023-09-19 16:59:09 -0300

    adjust font size for mobile

--- src/Components/MatchDetails.jsx
+++ src/Components/MatchDetails.jsx
@@ -5,13 +5,13 @@ const MatchDetails = ({ nextMatchData }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-2 max-lg:gap-0">
       <div>
-        <h1 className="text-5xl max-lg:text-3xl">
+        <h1 className="text-5xl max-lg:text-2xl text-center">
           {nextMatchData.time_mandante.nome} x{" "}
           {nextMatchData.time_visitante.nome}
         </h1>
       </div>
       <div>
-        <h5 className="text-xl max-lg:text-base whitespace-nowrap	">
+        <h5 className="text-xl max-lg:text-base whitespace-nowrap text-center">
           {nextMatchData.campeonato}
         </h5>
       </div>
