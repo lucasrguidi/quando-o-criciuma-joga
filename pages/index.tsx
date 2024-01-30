d3ab8c8fb8115a77addd1753f956b01ce4fa835d
Lucas Guidi <lucasrguidi@gmail.com>
2024-01-30 00:31:15 -0300

    perf: adicionado axios as requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import LastMatches from '../src/ui/Components/LastMatches'
 import NextMatch from '../src/ui/Components/NextMatch'
 import NextMatches from '../src/ui/Components/NextMatches'
@@ -18,19 +19,18 @@ interface ResponseData {
 export default function Home() {
   const [statsView, setStatsView] = useState<string>('table')
   const [loading, setLoading] = useState<boolean>(true)
-  const [data, setData] = React.useState<ResponseData | null>(null)
+  const [data, setData] = useState<ResponseData | null>(null)
 
   const apiUrl: string = process.env.NEXT_PUBLIC_API_URL as string
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function getData() {
       setLoading(true)
       try {
-        const response = await fetch(apiUrl)
-        const json = await response.json()
-        setData(json)
+        const response = await axios.get(apiUrl, { timeout: 10000 })
+        setData(response.data)
       } catch (error) {
-        return
+        console.error('Erro ao buscar os dados:', error)
       } finally {
         setLoading(false)
       }
@@ -50,7 +50,7 @@ export default function Home() {
           style={{ height: '95%' }}
         >
           <div className="w-1/2 h-full flex flex-col items-center justify-center p-4 max-lg:p-0 max-lg:w-full max-lg:h-full">
-            <div className="bg-white h-full gap-4 flex flex-col max-h-[80%] w-full rounded-2xl items-center p-4  max-lg:gap-4 max-lg:w-100 max-lg:h-screen ">
+            <div className="bg-white h-full gap-4 flex flex-col max-h-[90%] w-full rounded-2xl items-center p-4  max-lg:gap-4 max-lg:w-100 max-lg:h-screen ">
               <Selector
                 setStatsView={setStatsView}
                 statsView={statsView}
@@ -64,6 +64,7 @@ export default function Home() {
               {statsView === 'lastMatches' ? (
                 <LastMatches loading={loading} data={data?.lastMatchesData} />
               ) : null}
+              <span>teste teste</span>
             </div>
           </div>
           <div className="w-1/2 h-full flex items-center justify-center p-4 max-lg:p-0 max-lg:w-full max-lg:h-full ">
